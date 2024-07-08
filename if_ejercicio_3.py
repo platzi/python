24c2ff4464dedf98823c26f9a1cd40d4331b4a3b
Carla M. F. Román <43819418+carlicode@users.noreply.github.com>
2024-07-08 17:14:26 -0600

    Condicionales

---
+++ if_ejercicio_3.py
@@ -0,0 +1,10 @@
+is_member = True
+age = 11
+
+if is_member:
+    if age>=15:
+        print("Tienes acceso ya que eres miembro y mayor o igual a 15 años")
+    else:
+        print("No tienes acceso ya que eres miembro pero menor a 15 años")
+else:
+    print("No eres miembro y NO TIENES ACCESO")
