6895ebdefe9ad02bd820b00b424fe2246ae12a9a
Carla M. F. Román <43819418+carlicode@users.noreply.github.com>
2024-08-29 21:24:42 -0400

    Manipulación de Cadenas de Texto en Python

---
+++ cadenas.py
@@ -0,0 +1,9 @@
+name = 'CARLA Marcela'
+last_name = '   Florida     Roman  '
+print(5 * name)
+print(name + ' ' + last_name)
+print(len(name))
+print(len(last_name))
+print(name.lower())
+print(name.upper())
+print(last_name.strip())
