9e11b1a61fdda9f52581b9aed2149f3897be80d0
Carla M. F. Román <43819418+carlicode@users.noreply.github.com>
2024-08-29 21:28:35 -0400

    Enteros, Flotantes y Booleanos

---
+++ int_float_boolean.py
@@ -0,0 +1,15 @@
+x = 10
+y = 5.678
+#z = 1.2E6
+#a = 1.2E-6
+print(type(x))
+print(type(y))
+#print(z)
+#print(a)
+print(x + x)
+print(x + y)
+print(y + y)
+is_true = True
+is_false = False
+print(is_true)
+print(type(is_true))
