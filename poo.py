dc26ade709511319f0d80efd097159563f5631cb
Carla M. F. Román <43819418+carlicode@users.noreply.github.com>
2024-08-29 22:24:31 -0400

    Fundamentos de Programación Orientada a Objetos en Python

---
+++ poo.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print(f"Hola, mi nombre es {self.name} y tengo {self.age}")
+
+person1 = Person("Ana", 30)
+person2 = Person("Luis", 25)
+
+person1.greet()
+person2.greet()
